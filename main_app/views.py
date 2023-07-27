bbc03c2978ef3b4ba1cf7184c40c7b6d216f5de4
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2023-07-27 00:34:02 -0700

    HW pt2

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
+from .models import Dog
 
 # Create your views here.
 
-dogs = [
-  {'name': 'Bubbles', 'breed': 'French', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+
 # Define the home view
 def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
@@ -13,7 +11,8 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 def dogs_index(request):
-  # We pass data to a template very much like we did in Express!
-  return render(request, 'dogs/index.html', {
-    'dogs': dogs
-  })
+  dogs = Dog.objects.all()
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', { 'dog': dog })
