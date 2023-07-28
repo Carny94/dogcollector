6f890246916ee1e2dec4df22e49d206f991225c1
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2023-07-28 07:09:48 -0700

    left off at adding date feature

--- main_app/models.py
+++ main_app/models.py
@@ -2,12 +2,19 @@ from django.db import models
 # Import the reverse function
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 class Dog(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+
 
   # Changing this instance method
   # does not impact the database, therefore
@@ -17,3 +24,23 @@ class Dog(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'dog_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  date = models.DateField()
+  meal = models.CharField(
+  max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0]
+  )
+
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+
+  def __str__(self):
+     # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+
+
