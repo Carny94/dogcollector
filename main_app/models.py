e9e18a045d644946e25891745afbfe87de4a10a5
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2023-08-01 07:38:53 -0700

    pt-5 homework

--- main_app/models.py
+++ main_app/models.py
@@ -7,6 +7,15 @@ MEALS = (
     ('L', 'Lunch'),
     ('D', 'Dinner')
 )
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
 
 # Create your models here.
 class Dog(models.Model):
@@ -14,6 +23,7 @@ class Dog(models.Model):
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
 
 
   # Changing this instance method
@@ -45,3 +55,10 @@ class Feeding(models.Model):
 
 class Meta:
     ordering = ['-date']
+
+# class Photo(models.Model):
+#     url = models.CharField(max_length=200)
+#     dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return f"Photo for dog_id: {self.dog_id} @{self.url}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,12 @@
+import uuid
+import boto3
+from django.shortcuts import render, redirect
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dog
+from django.views.generic import ListView, DetailView
+# from .models import Dog Photo
 from .forms import FeedingForm
-
+from .models import Dog, Toy
 # Create your views here.
 
 
@@ -15,20 +19,26 @@ def about(request):
 def dogs_index(request):
   dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 'dogs': dogs })
+
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
+  id_list = dog.toys.all().values_list('id')
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in=id_list)
   feeding_form = FeedingForm()
   return render(request, 'dogs/detail.html', {
-    'dog': dog, 'feeding_form': feeding_form
+    'dog': dog, 
+    'feeding_form': feeding_form,
+    'toys': toys_dog_doesnt_have,
   })
   
 class DogCreate(CreateView):
   model = Dog
-  fields = '__all__'
+  fields = ['name', 'breed', 'description', 'age']
   # Special string pattern Django will use
   success_url = '/dogs/{dog_id}' # <--- must specify an exact ID
   # Or..more fitting... you want to just redirect to the index page
   success_url = '/dogs'
+
 class DogUpdate(UpdateView):
   model = Dog
   # Let's disallow the renaming of a cat by excluding the name field!
@@ -38,13 +48,41 @@ class DogDelete(DeleteView):
   model = Dog
   success_url = '/dogs'
 
-def add_feeding(request, cat_id):
+def add_feeding(request, dog_id):
   form = FeedingForm(request.POST)
    # validate the form
   if form.is_valid():
     # don't save the form to the db until it
     # has the cat_id assigned
     new_feeding = form.save(commit=False)
-    new_feeding.cat_id = cat_id
+    new_feeding.dog_id = dog_id
     new_feeding.save()
-  return redirect('detail', cat_id=cat_id)
+  return redirect('detail', dog_id=dog_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys'
+
+def assoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+def unassoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.remove(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+
